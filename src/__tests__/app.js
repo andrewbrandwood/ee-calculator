c5bfc7c1ae3be3d1e232fa602dc200f5b6a3ccb3
andrewbrandwood <andrew@brandwood.com>
2019-05-28 22:53:41 +0100

    tests: adding tests for state and components

--- src/__tests__/app.js
+++ src/__tests__/app.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../App';
 import Header from '../components/header/header';
+import Display from '../components/display/display';
+import Numpad from '../components/numpad/numpad';
 
 describe('when loading the app', () => {
   const wrapper = shallow(<App />);
@@ -12,6 +14,18 @@ describe('when loading the app', () => {
 
   it('renders header', () => {
     shallow(<Header />);
+  });
+
+  it('renders the calculator display', () => {
+    shallow(<Display />);
+  });
+
+  it('renders the number pad', () => {
+    shallow(<Numpad />);
+  })
+
+  it('should have a state object', () => {
+    expect(wrapper.state).toHaveLength(1);
   });
 
 });
