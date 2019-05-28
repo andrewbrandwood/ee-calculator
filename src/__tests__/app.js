a19514ef3823aa639e0844d622f0db263c4e0c8e
andrewbrandwood <andrew@brandwood.com>
2019-05-28 22:35:57 +0100

    tests: adding render test

--- src/__tests__/app.js
+++ src/__tests__/app.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../App';
+import Header from '../components/header/header';
 
 describe('when loading the app', () => {
   const wrapper = shallow(<App />);
 
   it('renders without crashing', () => {
     shallow(<App />);
+  });
+
+  it('renders header', () => {
+    shallow(<Header />);
   });
 
 });
